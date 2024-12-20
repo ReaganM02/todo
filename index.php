bfa31bad741a77cc9089488b42e00de8b7367735
Reagan Mahinay <reaganmahinay.web@gmail.com>
2024-12-20 08:51:28 +0800

    Added CSS

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php get_header();?>
-<main class="main-content">
-
-</main>
+  <main class="main-content">
+    <div class="todo-items">
+      Hello World
+    </div>
+  </main>
 <?php get_footer(); ?>
