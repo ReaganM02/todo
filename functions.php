2c2bc3dd721ab4dfd5afe892dc81c8cd6ffdee2d
Reagan Mahinay <reaganmahinay.web@gmail.com>
2024-12-20 08:51:21 +0800

    enqueue font css

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 add_action('wp_enqueue_scripts', function() {
   wp_enqueue_style('todo-main-css', get_theme_file_uri() . '/assets/styles/main.css');
+  wp_enqueue_style('todo-font-css', get_theme_file_uri() . '/assets/styles/fonts.css');
 })
 ?>
