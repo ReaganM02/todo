8dd9290a2ba7f5b2389e0f4926eab4962c9e0dc7
Reagan Mahinay <reaganmahinay.web@gmail.com>
2024-12-21 07:05:33 +0800

    Added security

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
+<?php
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly
+}
+require_once get_template_directory() . '/includes/todo.php';
+?>
+
 <?php get_header();?>
   <main class="main-content">
     <div class="todo-items">
-      Hello World
+      <?php require_once get_template_directory() . '/templates/form.php'  ?>
+      <?php require_once get_template_directory() . '/templates/list.php'  ?>
     </div>
   </main>
 <?php get_footer(); ?>
